32a99912b0d53ab5613f0034a187f905dcce4fc3
weilintsaiWand <wei-lin.tsai@wandisco.com>
2015-07-14 15:59:11 -0700

    Minor, refince coding stlye of XAttrPermissionFilter

--- giraffa-core/src/main/java/org/apache/giraffa/hbase/XAttrPermissionFilter.java
+++ giraffa-core/src/main/java/org/apache/giraffa/hbase/XAttrPermissionFilter.java
@@ -26,19 +26,18 @@ import org.apache.hadoop.hdfs.XAttrHelper;
 import org.apache.hadoop.security.AccessControlException;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 /**
- * copy from
+ * Copied and refined from
  * ${@link org.apache.hadoop.hdfs.server.namenode.XAttrPermissionFilter}
- * since that one is private
+ * Since that one is private
  */
 public class XAttrPermissionFilter {
   public XAttrPermissionFilter() {
   }
 
-  static void checkPermissionForApi( FSPermissionChecker pc, XAttr xAttr)
+  static void checkPermissionForApi(FSPermissionChecker pc, XAttr xAttr)
       throws AccessControlException {
     if(xAttr.getNameSpace() != XAttr.NameSpace.USER &&
        (xAttr.getNameSpace() != XAttr.NameSpace.TRUSTED || !pc.isSuperUser())) {
@@ -49,38 +48,22 @@ public class XAttrPermissionFilter {
 
   static void checkPermissionForApi(FSPermissionChecker pc, List<XAttr> xAttrs)
       throws AccessControlException {
-    Preconditions.checkArgument(xAttrs != null);
-    if(!xAttrs.isEmpty()) {
-      Iterator i$ = xAttrs.iterator();
-
-      while(i$.hasNext()) {
-        XAttr xAttr = (XAttr)i$.next();
-        checkPermissionForApi(pc, xAttr);
-      }
+    Preconditions.checkNotNull(xAttrs);
+    for (XAttr xAttr : xAttrs) {
+      checkPermissionForApi(pc, xAttr);
     }
   }
 
   static List<XAttr> filterXAttrsForApi(FSPermissionChecker pc,
                                         List<XAttr> xAttrs) {
-    assert xAttrs != null : "xAttrs can not be null";
-
-    if(xAttrs != null && !xAttrs.isEmpty()) {
-      ArrayList filteredXAttrs = Lists.newArrayListWithCapacity(xAttrs.size());
-      Iterator i$ = xAttrs.iterator();
-
-      while(i$.hasNext()) {
-        XAttr xAttr = (XAttr)i$.next();
-        if(xAttr.getNameSpace() == XAttr.NameSpace.USER) {
-          filteredXAttrs.add(xAttr);
-        } else if(xAttr.getNameSpace() ==
-                  XAttr.NameSpace.TRUSTED && pc.isSuperUser()) {
-          filteredXAttrs.add(xAttr);
-        }
+    Preconditions.checkNotNull(xAttrs);
+    ArrayList filteredXAttrs = Lists.newArrayListWithCapacity(xAttrs.size());
+    for (XAttr xAttr : xAttrs) {
+      if(xAttr.getNameSpace() == XAttr.NameSpace.USER ||
+        (xAttr.getNameSpace() == XAttr.NameSpace.TRUSTED && pc.isSuperUser())) {
+        filteredXAttrs.add(xAttr);
       }
-
-      return filteredXAttrs;
-    } else {
-      return xAttrs;
     }
+    return filteredXAttrs;
   }
 }
